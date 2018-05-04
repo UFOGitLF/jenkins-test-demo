f5aa407d3e6f431cf62b1ec7bca162d1a75a0e44
UFOsubject <you@example.com>
2018-05-04 11:26:58 +0800

    'fix'

--- src/main/java/com/fly/controller/TestController.java
+++ src/main/java/com/fly/controller/TestController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello(){
-
         return "ok";
     }
 }
